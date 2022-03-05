8b0e336e860254b414f3a945d51ce705017ce087
Chavez Harris <codedbychavez@gmail.com>
2022-03-05 16:43:07 -0400

    Adopted proper naming conventions for modules

--- devops/hello_world/apps.py
+++ devops/hello_world/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class HelloworldConfig(AppConfig):
+class hello_worldConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'HelloWorld'
+    name = 'hello_world'
--- devops/urls.py
+++ devops/urls.py
@@ -1,4 +1,4 @@
-"""DevOps URL Configuration
+"""devops URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('HelloWorld',include('DevOps.HelloWorld.urls')),
+    path('hello-world',include('devops.hello_world.urls')),
     path('admin/', admin.site.urls),
 ]
