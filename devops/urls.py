6acd9d45eb6c9fd691bf0de11555fb35b094dd69
codedbychavez <codedbychavez@gmail.com>
2022-03-07 10:30:19 -0400

    added API test cases

--- devops/urls.py
+++ devops/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('hello-world',include('devops.hello_world.urls')),
+    path('hello-world/',include('devops.hello_world.urls')),
     path('admin/', admin.site.urls),
 ]
